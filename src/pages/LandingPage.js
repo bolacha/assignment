5f9e8d6739c826b11a38bdd74a0a60a050409bd0
Lucas Costa Oliveira <lucas@ulcas.com>
2017-11-06 21:01:29 -0200

    Added the last dom placements

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,43 @@
 import React, { Component } from 'react';
 
 export default class LandingPage extends Component {
+
+
+    renderProducts() {
+
+        const products = [
+            {
+                image : '/assets/product.png',
+                name  : 'iPhone 6 Black',
+                price : 'AED 1000'
+            },
+            {
+                image : '/assets/product.png',
+                name  : 'iPhone 7 Black',
+                price : 'AED 1000'
+            },{
+                image : '/assets/product.png',
+                name  : 'iPhone 8 Black',
+                price : 'AED 1000'
+            }
+        ];
+
+        return products.map((e) => {
+            return (
+                <div key={e.name} className="col-3">
+                    <img className="card-img-top product_image" src={e.image} alt="Card image cap"></img>
+                    <div className="card-block">
+                        <p className="product_name">{e.name}</p>
+                        <p className="product_price">{e.price}</p>
+                    </div>
+                </div>
+            );
+        });
+
+
+    }
+
+
     render() {
         return(
             <div className="container">
@@ -22,50 +59,36 @@ export default class LandingPage extends Component {
                     </div>
                 </nav>
 
+                <div className="row justify-content-center">
 
-                <form className="form-signin">
-                    <h2 className="form-signin-heading">Please sign in</h2>
-                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address"></input>
-                    <label htmlFor="inputPassword" className="sr-only">Password</label>
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password"></input>
-                    <div className="checkbox">
-                        <label>
-                            <input type="checkbox" value="remember-me"></input> Remember me
-                        </label>
-                    </div>
-                    <div className="forgot-password">
-                        <p>Forgot password?</p>
-                    </div>
-                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                </form>
+                        <form className="form-signin">
+                            <h2 className="form-signin-heading text-center">Please sign in</h2>
+                            <p className="form-signin-sub-heading text-center">Unlock Awesome Features!</p>
+
+                            <label htmlFor="inputEmail">Email address</label>
+                            <input type="email" id="inputEmail" className="form-control" placeholder="Email address"></input>
+                            <label htmlFor="inputPassword">Password</label>
+                            <input type="password" id="inputPassword" className="form-control" placeholder="Password"></input>
+                            <div className="checkbox">
+                                <div className="row">
+                                    <label htmlFor="remember-me" className="col-7">
+                                        <input type="checkbox" name="remember-me" id="inlineCheckbox1" value="option1"></input> Keep me logged in
+                                    </label>
+                                    <a className="col-5">Forgot password?</a>
+                                </div>
+
+
+
+                            </div>
+                            <button className="btn btn-lg btn-primary btn-block form-signin-submit" type="submit">SIGN IN</button>
+                        </form>
+
+                </div>
 
                 <hr/>
 
                 <div className="row justify-content-around">
-                    <div className="col-3">
-                        <img className="card-img-top" src="/assets/product.png" alt="Card image cap"></img>
-                        <div className="card-block">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="card-block">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="card-block">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-
+                    { this.renderProducts() }
                 </div>
 
             </div>
